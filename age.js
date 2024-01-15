d5d842139f3c910c33a2b979330ea75aa18a33cc
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2024-01-15 11:11:54 -0500

    Create age.js

---
+++ age.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() 
+        {
+            let birthdate = prompt("Please enter your birthdate (YYYY-MM-DD):");
+
+            birthdate = new Date(birthdate);
+            if (isNaN(birthdate)) 
+            {
+                document.getElementById("age").textContent = "Invalid Date";
+                return;
+            }
+
+            function calculateAge() 
+            {
+                const currentTime = new Date();
+                const ageInMilliseconds = currentTime - birthdate;
+                const ageInSeconds = ageInMilliseconds / 1000;
+                const ageInDecimal = ageInSeconds / 31536000;
+
+                document.getElementById("age").textContent = ageInDecimal.toFixed(10);
+            }
+
+            setInterval(calculateAge, 100);
+        });
